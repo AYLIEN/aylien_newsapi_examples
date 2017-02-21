d093ffc8e889ec1cfd01122ea5e25bb6805878d1
Hamed R. Nik <hamed.r.nik@gmail.com>
2017-02-21 17:03:06 +0000

    Add Javascript examples for getting started

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Here you can find snippet codes in several languages such as Ruby, Python, Javas
 Sign up at [https://newsapi.aylien.com/](https://newsapi.aylien.com/).
 
 For more documentation see [https://newsapi.aylien.com/docs/](https://newsapi.aylien.com/docs/).
+
+Note: Make sure you replace the YOUR_APP_ID and YOUR_APP_KEY placeholders with your own API credentials. Again, if you haven’t got an API account you can sign up [here](https://newsapi.aylien.com/).
---
+++ getting_started/nodejs.js
@@ -0,0 +1,37 @@
+var AylienNewsApi = require('aylien-news-api');
+
+var apiInstance = new AylienNewsApi.DefaultApi();
+
+// Configure API key authorization: app_id
+var app_id = apiInstance.apiClient.authentications['app_id'];
+app_id.apiKey = "YOUR_APP_ID";
+
+// Configure API key authorization: app_key
+var app_key = apiInstance.apiClient.authentications['app_key'];
+app_key.apiKey = "YOUR_APP_KEY";
+
+var opts = {
+  'title': 'trump',
+  'sortBy': 'social_shares_count.facebook',
+  'language': ['en'],
+  'notLanguage': ['es', 'it'],
+  'publishedAtStart': 'NOW-7DAYS',
+  'publishedAtEnd': 'NOW', 
+  'entitiesBodyLinksDbpedia': [
+    'http://dbpedia.org/resource/Donald_Trump',
+    'http://dbpedia.org/resource/Hillary_Rodham_Clinton'
+  ]
+};
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ');
+    console.log('========================================');
+    for (var i = 0; i < data.stories.length; i++){
+      console.log(data.stories[i].title + " / " + data.stories[i].source.name);
+    }
+  }
+};
+apiInstance.listStories(opts, callback);
