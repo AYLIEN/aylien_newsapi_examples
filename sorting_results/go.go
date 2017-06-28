de51cde82531d1fe98906c040639ef3f9dbf8c90
Hamed R. Nik <hamed.r.nik@gmail.com>
2017-06-28 14:47:31 +0100

    Fix alignment issue

--- sorting_results/go.go
+++ sorting_results/go.go
@@ -16,15 +16,13 @@ func main() {
 	api.Configuration.APIKeyPrefix["X-AYLIEN-NewsAPI-Application-Key"] = "{{current_app_key}}"
 
 	storiesParams := &newsapi.StoriesParams{
-		Language:         []string{"en"},
-		PublishedAtStart: "NOW-1MONTH",
-		PublishedAtEnd:   "NOW",
-		EntitiesBodyLinksDbpedia: []string{
-			"http://dbpedia.org/resource/Baseball"},
-		CategoriesTaxonomy: "iab-qag",
-		CategoriesId: []string{
-			"IAB17"},
-		SortBy: "source.rankings.alexa.rank.US"}
+		Language:                 []string{"en"},
+		PublishedAtStart:         "NOW-1MONTH",
+		PublishedAtEnd:           "NOW",
+		EntitiesBodyLinksDbpedia: []string{"http://dbpedia.org/resource/Baseball"},
+		CategoriesTaxonomy:       "iab-qag",
+		CategoriesId:             []string{"IAB17"},
+		SortBy:                   "source.rankings.alexa.rank.US"}
 
 	storiesResponse, res, err := api.ListStories(storiesParams)
 	if err != nil {
